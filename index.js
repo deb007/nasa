d08b12da9bbabe9e315e8e3c5703e3cbe2c27316
Debjyoti Aich <debjaich@in.ibm.com>
2018-05-22 11:39:29 +0530

    Added JSON parse

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ function getPOTD(params) {
     var str = (params && params.text) ? params.text : 'Hello World';
     
     return new Promise(function(resolve, reject) {
-        request('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo', function (error, response, body) {
+        request('https://api.nasa.gov/planetary/apod?api_key=KRpBIgwun3xR1eh7zIDkK4UOkfQy8K1hmZrKrdh9', function (error, response, body) {
             if(error) {
                 reject({err: error});
-            }            
-            resolve(body);
+            }       
+                 
+            resolve(JSON.parse(body));
         });
     })
 }
